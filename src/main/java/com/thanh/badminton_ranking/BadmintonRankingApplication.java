170ef1fa8a9716cd31cb443b6e7ee73b6228988d
Dang Thanh <thanhdd34@gmail.com>
2026-06-24 22:57:17 +0700

    chore: initialize Spring Boot project

---
+++ src/main/java/com/thanh/badminton_ranking/BadmintonRankingApplication.java
@@ -0,0 +1,13 @@
+package com.thanh.badminton_ranking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadmintonRankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadmintonRankingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thanh/badminton_ranking/BadmintonRankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thanh.badminton_ranking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadmintonRankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
